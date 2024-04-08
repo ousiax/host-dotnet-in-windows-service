20ed81f265faca1dc6816d12889d5908e2d32537
Jon X <ousiax@hotmail.com>
2024-04-08 10:42:43 +0800

    Host ASP.NET Core in a Windows Service

--- src/Example.WebApp/Program.cs
+++ src/Example.WebApp/Program.cs
@@ -1,6 +1,32 @@
 // Program.cs
 
-WebApplicationBuilder builder = WebApplication.CreateBuilder(args);
+using Microsoft.Extensions.Hosting.WindowsServices;
+using Microsoft.Extensions.Logging.Configuration;
+using Microsoft.Extensions.Logging.EventLog;
+
+// See https://github.com/dotnet/AspNetCore.Docs/issues/23387#issuecomment-927317675
+WebApplicationOptions options = new()
+{
+    Args = args,
+    // Sets the content root to AppContext.BaseDirectory.
+    ContentRootPath = WindowsServiceHelpers.IsWindowsService() ? AppContext.BaseDirectory : default
+};
+
+WebApplicationBuilder builder = WebApplication.CreateBuilder(options);
+
+// Sets the host lifetime to WindowsServiceLifetime.
+builder.Services.AddWindowsService(options =>
+{
+    options.ServiceName = ".NET Example WebApp";
+});
+
+builder.Logging.AddEventLog(options =>
+{
+    options.SourceName = ".NET Example WebApp";
+});
+
+LoggerProviderOptions.RegisterProviderOptions<EventLogSettings, EventLogLoggerProvider>(builder.Services);
+
 WebApplication app = builder.Build();
 
 app.MapGet("/", () => "Hello World!");
