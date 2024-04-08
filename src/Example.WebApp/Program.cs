090159882eaa7b821a15f47691e1968eda1c7efa
Jon X <ousiax@hotmail.com>
2024-04-08 10:35:14 +0800

    .NET Generic Host

---
+++ src/Example.WebApp/Program.cs
@@ -0,0 +1,8 @@
+// Program.cs
+
+WebApplicationBuilder builder = WebApplication.CreateBuilder(args);
+WebApplication app = builder.Build();
+
+app.MapGet("/", () => "Hello World!");
+
+app.Run();
---
+++ src/Example.WorkerService/Program.cs
@@ -0,0 +1,18 @@
+﻿// Program.cs
+
+HostApplicationBuilder builder = Host.CreateApplicationBuilder(args);
+builder.Services.AddHostedService<Worker>();
+using IHost host = builder.Build();
+host.Run();
+
+public sealed class Worker(ILogger<Worker> logger) : BackgroundService
+{
+    protected override async Task ExecuteAsync(CancellationToken stoppingToken)
+    {
+        while (!stoppingToken.IsCancellationRequested)
+        {
+            await Task.Delay(TimeSpan.FromSeconds(5), stoppingToken);
+            logger.LogInformation("{}: I'm busy at work.", DateTime.Now.TimeOfDay);
+        }
+    }
+}
