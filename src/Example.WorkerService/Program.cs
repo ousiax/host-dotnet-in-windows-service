b0c6ab9a1f58a019a141532fc8c4c9271d5eb47b
Jon X <ousiax@hotmail.com>
2024-04-08 10:41:09 +0800

    Create Windows Service using BackgroundService

--- src/Example.WorkerService/Program.cs
+++ src/Example.WorkerService/Program.cs
@@ -1,6 +1,21 @@
 ﻿// Program.cs
 
+using Microsoft.Extensions.Logging.Configuration;
+using Microsoft.Extensions.Logging.EventLog;
+
 HostApplicationBuilder builder = Host.CreateApplicationBuilder(args);
+builder.Services.AddWindowsService(options =>
+{
+    options.ServiceName = ".NET Example WorkerService";
+});
+
+builder.Logging.AddEventLog(options =>
+{
+    options.SourceName = ".NET Example WorkerService";
+});
+
+LoggerProviderOptions.RegisterProviderOptions<EventLogSettings, EventLogLoggerProvider>(builder.Services);
+
 builder.Services.AddHostedService<Worker>();
 using IHost host = builder.Build();
 host.Run();
@@ -9,10 +24,32 @@ public sealed class Worker(ILogger<Worker> logger) : BackgroundService
 {
     protected override async Task ExecuteAsync(CancellationToken stoppingToken)
     {
-        while (!stoppingToken.IsCancellationRequested)
+        try
         {
-            await Task.Delay(TimeSpan.FromSeconds(5), stoppingToken);
-            logger.LogInformation("{}: I'm busy at work.", DateTime.Now.TimeOfDay);
+            while (!stoppingToken.IsCancellationRequested)
+            {
+                await Task.Delay(TimeSpan.FromSeconds(5), stoppingToken);
+                logger.LogInformation("{}: I'm busy at work.", DateTime.Now.TimeOfDay);
+            }
+        }
+        catch (OperationCanceledException)
+        {
+            // When the stopping token is canceled, for example, a call made from services.msc,
+            // we shouldn't exit with a non-zero exit code. In other words, this is expected...
+        }
+        catch (Exception ex)
+        {
+            logger.LogError(ex, "{Message}", ex.Message);
+
+            // Terminates this process and returns an exit code to the operating system.
+            // This is required to avoid the 'BackgroundServiceExceptionBehavior', which
+            // performs one of two scenarios:
+            // 1. When set to "Ignore": will do nothing at all, errors cause zombie services.
+            // 2. When set to "StopHost": will cleanly stop the host, and log errors.
+            //
+            // In order for the Windows Service Management system to leverage configured
+            // recovery options, we need to terminate the process with a non-zero exit code.
+            Environment.Exit(1);
         }
     }
 }
